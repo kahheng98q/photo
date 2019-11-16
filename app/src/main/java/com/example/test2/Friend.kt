24ddd56fdea601ad3a48681b4551e0ad973cfa11
kahheng <kahheng98q@gmail.com>
2019-11-16 22:48:33 +0800

    proto

---
+++ app/src/main/java/com/example/test2/Friend.kt
@@ -0,0 +1,6 @@
+package com.example.test2
+
+
+
+
+data class Friend(val imageint: Int,val name: String, val address: String)
---
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.test2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        linearLayoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = linearLayoutManager
+        val friend = ArrayList<Friend>()
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = RecycleAdapter(friend)
+
+        recyclerView = findViewById<RecyclerView>(R.id.rv_friend_list).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+    }
+
+}
+
+}
